ed0d928d6639f76e695a51955920a5c1fc274bdb
Vin Tang <vin@Vins-MBP.home>
2021-03-14 10:13:12 -0400

    add more datasets

--- app.py
+++ app.py
@@ -30,19 +30,39 @@ def peek(src, dst, k):
 st.title("Peek")
 st.write('A no-fuss way to downsample datasets.')
 
-options = ['activeloop/mnist', 'activeloop/cifar10_train', "activeloop/cifar100_train", "activeloop/omniglot_test"]
+options = [
+    'activeloop/mnist', 
+    'activeloop/cifar10_train', 
+    'activeloop/cifar100_train', 
+    'activeloop/omniglot_test',
+    'activeloop/objectron_camera',
+    'activeloop/lm1b',
+    'activeloop/twitter_airline',
+    'activeloop/CoLA',
+    'activeloop/dota',
+    'activeloop/kitti_train',
+    'activeloop/deep_weeds_train',
+    'activeloop/voc_2012_train',
+    'activeloop/food101_train',
+    'activeloop/cars196_test',
+    'activeloop/eurosat_train',
+    'activeloop/coil100_train',
+    'activeloop/caltech_birds2010_train',
+    'activeloop/beans_train']
+
 datasets = st.multiselect("Dataset", options)
 
 if datasets:
-    subset = st.slider('Keep %', min_value=0.0, max_value=1.0, value=0.0, step=0.05)
+    subset = st.slider('Fraction to keep', min_value=0.0, max_value=1.0, value=0.0, step=0.05)
     if subset != 0:
         st.write('Slicing', subset)
-        tag = "mynameisvinn/{}".format(uuid.uuid1())
-        peek(src=datasets[0], dst=tag, k=subset)
-        
-        st.write("Code snippet")
-        body = f"""
-            import hub            
-            ds = hub.load('{tag}')
-            """.format(tag)
-        st.code(body, language='python')
+        tag = "mynameisvinn/{}-{}".format(uuid.uuid1(), subset)
+        if peek(src=datasets[0], dst=tag, k=subset):
+            st.write("Code snippet")
+            body = f"""
+                import hub            
+                ds = hub.load('{tag}')
+                """.format(tag)
+            st.code(body, language='python')
+        else:
+            st.write("Slicing failed.")
